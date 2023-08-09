036854e68ccc12c4d22d08f871f2758d6a0d99bb
Ayush <ayushjpeg@gmail.com>
2023-08-09 11:25:26 +0000

    host change

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    # Run the app on all network interfaces on port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
