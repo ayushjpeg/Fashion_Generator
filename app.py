a68998b1a5beca1673f8d89a707c91e656a8a873
Ayush <ayushjpeg@gmail.com>
2023-08-09 21:52:00 +0000

    chatbot changes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import random, os, sys
 from flask import Flask, redirect, render_template, request
 import nltk
 from nltk.chat.util import Chat, reflections
+from support import Fashion_array
+import re
 
 # Configure application
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -13,21 +15,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Variables
 entries = []
-user_preferences = {
-    'color': None,
-    'clothing': None,
-    'brands': None,
-    'style': None
-}
-fashion_patterns = [
-    (r'hi|hello', ['Hello!', 'Hi there!', 'Hey!']),
-    (r'bye|goodbye', ['Goodbye!', 'See you later!', 'Bye!']),
-    (r'my favorite color is (.*)', ['That\'s a nice color choice!', 'I like that color too.']),
-    (r'I like to wear (.*)', ['That\'s a great choice of clothing!', 'Nice preference!']),
-    (r'I prefer (.*) brands', ['Brands are important! Which brands do you like?', 'Tell me more about your favorite brands.']),
-    (r'I love (.*) style', ['Styles say a lot about a person! Could you elaborate on that?', 'Interesting! What do you like about that style?']),
-    (r'(.*)', ['I hear you. Tell me more about fashion.', 'Fashion is fascinating! Could you tell me more?'])
-]
+user_preferences = {}
+fashion_patterns = Fashion_array()
 
 # Chatbot config
 fashion_chatbot = Chat(fashion_patterns, reflections)
@@ -46,10 +35,43 @@ def index():
     if request.method == "POST":
         s = request.form.get("Entry")
         entries.append(s)
-        print(s)
         response_of_bot = fashion_chatbot.respond(str(s))
         entries.append(response_of_bot)
-        print(entries)
+        try:
+            for pattern, _ in fashion_patterns:
+                match = nltk.re.search(pattern, str(s))
+                if match:
+                    preference = match.group(1)
+                    if preference:
+                        # Determine the category based on the pattern used
+                        if 'wear'  in pattern:
+                            category = 'clothing'
+                        elif 'accessory' in pattern:
+                            category = 'accessories'
+                        elif 'foot' in pattern:
+                            category = 'footwear'
+                        elif 'color' in pattern:
+                            category = 'colors'
+                        elif 'material' in pattern:
+                            category = 'materials'
+                        elif 'style' in pattern:
+                            category = 'styles'
+                        elif 'brand' in pattern:
+                            category = 'brands'
+                        else:
+                            category = 'other'  # Modify this as needed
+                            
+                        if category not in user_preferences:
+                            user_preferences[category] = []
+                        user_preferences[category].append(preference)  # Store the preference in the dictionary
+                        
+                        entries.append(f"I'll remember that you enjoy {preference} in {category}.")
+                    break
+            
+        except:
+            entries.append("I was expecting something more detailed about your likings.")
+        print(user_preferences)
+            
         return render_template("index.html", entries=entries)
 
     else:
