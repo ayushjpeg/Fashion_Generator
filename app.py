e0ef8cb3d32553a9acd264ee0d4085c17839dd83
Ayush <ayushjpeg@gmail.com>
2023-08-09 13:07:12 +0000

    stylesupdate

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def index():
 
 if __name__ == '__main__':
     # Run the app on all network interfaces on port 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
