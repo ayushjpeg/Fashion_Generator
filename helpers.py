d62dc8ad83b32bde20c28e3722b5e3f1ca49a97d
Ayush <ayushjpeg@gmail.com>
2023-08-12 09:13:02 +0000

    login-2

--- helpers.py
+++ helpers.py
@@ -1,19 +1,9 @@
-import os
-import requests
-import urllib.parse
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
 
 def apology(message, code=400):
-    """Render message as an apology to user."""
     def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
@@ -22,11 +12,6 @@ def apology(message, code=400):
 
 
 def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
